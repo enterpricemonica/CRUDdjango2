bef529b593bdb2c0bf87831369753f57c2145349
Monica Santamaria <santamariamonica31@gmail.com>
2024-06-10 21:18:52 -0500

    3_Tercer: Configuracion Pagina Inicio

--- inventario/inventario/urls.py
+++ inventario/inventario/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 from productos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hola-mundo/', views.holaMundo,name="holaMundo" ),
+    path('', include('productos.urls')),
 ]
